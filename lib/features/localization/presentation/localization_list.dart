571c19251d9ee6dfa4e204fd8fb6615aad178fae
mklusovich <mklusovich@datamola.com>
2023-12-03 20:48:33 +0100

    add localization

---
+++ lib/features/localization/presentation/localization_list.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:game_box/core/presentation/entities/locale.entity.dart';
+import 'package:game_box/core/presentation/shared/image_select_item/image_select_item.dart';
+import 'package:game_box/features/localization/domain/bloc/localization.bloc.dart';
+import 'package:game_box/features/localization/domain/bloc/localization.events.dart';
+
+class LocalizationList extends StatelessWidget {
+  final LocaleEnum localization;
+
+  const LocalizationList({super.key, required this.localization});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ImageSelectItem(
+          imgPath: 'assets/images/english.png',
+          text: 'English',
+          isSelected: localization == LocaleEnum.eng,
+          onTap: () {
+            context.read<LocalizationBloc>().add(const LocalizationChange(LocaleEnum.eng));
+          },
+        ),
+        const SizedBox(
+          height: 16,
+        ),
+        ImageSelectItem(
+          imgPath: 'assets/images/russia.png',
+          text: 'Russian',
+          isSelected: localization == LocaleEnum.ru,
+          onTap: () {
+            context.read<LocalizationBloc>().add(const LocalizationChange(LocaleEnum.ru));
+          },
+        ),
+      ],
+    );
+  }
+}
